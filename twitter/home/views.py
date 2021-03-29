6d2fecd21179342bf28f3c96692cb0e3fcea962a
Jason Friedman <jason@Jasons-MBP-5.fios-router.home>
2021-03-28 22:06:09 -0400

    final changes 7.0

--- twitter/home/views.py
+++ twitter/home/views.py
@@ -62,6 +62,8 @@ def profile(request):
 
     return render(request, 'profilepage.html', {'tweets' : tweets, 'liked_tweets' : liked_tweets, 'my_tweets' : my_tweets, 'username' : username, 'tweet_count' : tweet_count, 'append' : append })
 
+def home_view();
+    pass
 
 def home(request):
     if not request.user.is_authenticated:
